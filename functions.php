75284227f19ddcb6338fea103e54a582e4a2b5ab
megumiru-morifuji <153786014+megumiru-morifuji@users.noreply.github.com>
2025-01-21 14:53:07 +0900

    Update functions.php
    ブログのクラシックエディタ非表示にした

--- functions.php
+++ functions.php
@@ -14,6 +14,16 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
 }
 
+touch( get_stylesheet_directory() . '/page-front.php' );
+touch( get_stylesheet_directory() . '/front-page.php' );
+touch( get_stylesheet_directory() . '/single-blog.php' );
+
+// googleフォントの読み込み
+function add_google_fonts() {
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap', array(), null );
+}
+add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
+
 // // コメント機能を完全に無効化
 function disable_comments_completely() {
     // 投稿タイプ全てでコメントを無効化
@@ -49,3 +59,21 @@ function remove_comments_admin_bar() {
     $wp_admin_bar->remove_menu('comments');
 }
 add_action('wp_before_admin_bar_render', 'remove_comments_admin_bar');
+
+// カスタム投稿タイプ'your_post_type'でクラシックエディタを無効化
+function disable_classic_editor_for_custom_post($use_block_editor, $post_type) {
+    // カスタム投稿タイプの名前を指定
+    if ($post_type === 'your_post_type') {
+        // ブロックエディタを強制的に使用
+        return true;
+    }
+    return $use_block_editor;
+}
+add_filter('use_block_editor_for_post_type', 'disable_classic_editor_for_custom_post', 10, 2);
+
+// クラシックエディタの切り替えオプションを非表示
+function remove_classic_editor_options() {
+    $post_type = 'blog'; // カスタム投稿タイプの名前を指定
+    remove_post_type_support($post_type, 'editor');
+}
+add_action('init', 'remove_classic_editor_options');
