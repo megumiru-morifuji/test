d9d4ad4e39d1bd9bd214a2f65bedd4a905cd2590
megumiru-morifuji <153786014+megumiru-morifuji@users.noreply.github.com>
2025-02-28 13:30:53 +0900

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -7,9 +7,61 @@ Template Name: フロントページ
 <?php get_header(); ?>
 
 <main id="main" class="site-main">
-	<div class="mainimge">
-		<img src="http://megumiru.jp/wine/wp-content/uploads/2025/01/art-7917562_1280.jpg">
+	<div class="main-slide">
+		<div class="mainimge">
+			<img src="http://megumiru.jp/wine/wp-content/uploads/2025/01/art-7917562_1280.jpg">
+		</div>
+	<div class="box-text">
+		<h3>ワインと絵画を融合させた<br>新しいライフスタイル</h3>
+    </div>
 	</div>
+	
+	<!-- ブログ最新記事セクション -->
+<section class="blog-latest-posts">
+	<div class="ttl"><h2>View</h2></div>
+    <div class="blog-wrap">
+		<div class="posts-grid">
+        <?php
+        $args = array(
+            'post_type' => 'blog', // カスタム投稿タイプ名
+            'posts_per_page' => 6, // 表示する記事数
+            'orderby' => 'date',   // 日付順
+            'order' => 'DESC'      // 降順（最新順）
+        );
+        $blog_query = new WP_Query($args);
+        
+        if ($blog_query->have_posts()) :
+            while ($blog_query->have_posts()) : $blog_query->the_post();
+        ?>
+            <article class="post-card">
+                <?php if (get_field('mainimg')) : ?>
+                    <div class="post-thumbnail">
+                        <img src="<?php the_field('mainimg'); ?>" alt="<?php the_title(); ?>">
+                    </div>
+                <?php endif; ?>
+                <div class="post-content">
+                    <div class="post-meta">
+                        <time datetime="<?php echo get_the_date('c'); ?>">
+                            <?php echo get_the_date('Y.m.d'); ?>
+                        </time>
+                    </div>
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <?php if (get_field('supplement')) : ?>
+                        <div class="post-excerpt">
+                            <?php echo wp_trim_words(get_field('supplement'), 40); ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </article>
+        <?php
+            endwhile;
+            wp_reset_postdata();
+        endif;
+        ?>
+    </div>
+	</div>
+    
+</section>
     <div class="container">
 <!--         <!-- ヒーローセクション -->
 <!--         <section class="hero-section">
