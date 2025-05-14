e61aab34be26505e1a9589ac56af33c41ddddfeb
megumiru-morifuji <153786014+megumiru-morifuji@users.noreply.github.com>
2025-05-14 11:17:49 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="section siteFooter">
+			
+	
+	<div class="container sectionBox copySection text-center">
+			<p>Copyright © Paint＆Sipマガジン All Rights Reserved.</p><p>Powered by <a href="https://wordpress.org/">WordPress</a> &amp; <a href="https://lightning.nagoya/ja/" target="_blank" title="Free WordPress Theme Lightning"> Lightning Theme</a> by Vektor,Inc. technology.</p>	</div>
+</footer>
